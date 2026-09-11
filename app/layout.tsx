652fd7fdd4a6a2cacb9243cfd77a9fa02485795c
Sebluimeme <bmsyoder@gmail.com>
2026-09-11 15:33:52 +0200

    fix: rendre l'injection GTM idempotente (race hydratation)

--- app/layout.tsx
+++ app/layout.tsx
@@ -143,7 +143,8 @@ gtag('consent','default',{'ad_storage':granted,'ad_user_data':granted,'ad_person
           id="gtm-script"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            __html: `(function(w,d,s,l,i){if(w.__gtmInjected)return;w.__gtmInjected=true;
+w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
